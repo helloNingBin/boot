6f0ea96df76169f7251273d53ba329a9ce03774e
ning <helloningbin@gmail.com>
2021-02-05 15:36:12 +0800

    commint from company

--- src/main/java/com/example/boot/BootApplication.java
+++ src/main/java/com/example/boot/BootApplication.java
@@ -9,6 +9,7 @@ public class BootApplication {
     public static void main(String[] args) {
         System.out.println("-------------begine start bootapplication-------------");
         SpringApplication.run(BootApplication.class, args);
+        //------
     }
 
 }
